36f4abf8a06c4bdb4349717405dcefd48bcedd33
hy072586 <98764128+hy072586@users.noreply.github.com>
2022-02-03 16:34:49 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -12,7 +12,7 @@ def welcome_assignment_answers(question):
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "No"
+        answer = "Yes"
     return(answer)
 # Complete all the questions.
 
