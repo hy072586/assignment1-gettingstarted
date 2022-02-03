3faa754539acbc6fe01800309414e2ee1c9bd8c2
hy072586 <98764128+hy072586@users.noreply.github.com>
2022-02-03 16:35:41 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -13,6 +13,9 @@ def welcome_assignment_answers(question):
         answer = "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "Yes"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "Yes"
+
     return(answer)
 # Complete all the questions.
 
